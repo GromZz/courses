e09d53bdc944afdaec32f191e748b2893be4304c
GromZz <81381951+GromZz@users.noreply.github.com>
2021-06-03 21:50:32 +0300

    Update competency.php

--- development/apps/courses/model/config/methods/competency.php
+++ development/apps/courses/model/config/methods/competency.php
@@ -1,41 +1,5 @@
 <?php
 $methods = [
-	'add' => [
-		'params' => [
-			[
-				'name' => 'discipline',
-				'source' => 'p',
-				'pattern' => '',
-				'required' => true
-			],
-            [
-				'name' => 'description',
-				'source' => 'p',
-				'pattern' => '',
-				'required' => true
-            ]
-		]		
-	],
-	'delete' => [
-		'params' => [
-			[
-				'name' => 'discipline',
-				'source' => 'p',
-				'pattern' => '',
-				'required' => true
-			]
-		]
-	],
-	'get' => [
-		'params' => [
-			[
-				'name' => 'discipline',
-				'source' => 'p',
-				'pattern' => '',
-				'required' => true
-			]
-		]
-	],
 	'edit' => [
 		'params' => [
 			[
